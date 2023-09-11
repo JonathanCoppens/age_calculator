dbe90a507a6434e605dc4b02749a135504a8bf08
Jonathan Coppens <jonathan.b.coppens@gmail.com>
2023-09-11 17:29:02 +0200

    _

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
-let addDays = function(date, days) {
+/* let addDays = function(date, days) {
     const newDate = new Date(date.getTime());
     newDate.setDate(newDate.getDate() + days);
     return newDate;
 };
 
 
-
-let yourAge = function() {
-    newDate
+ */
+function calculateAge(birthday) {
+    const today = new Date();
+    const birthdate = new Date(birthday.value);
+    const age = today.getFullYear() - birthdate.getFullYear() -
+        (today.getMonth() < birthdate.getMonth() ||
+        (today.getMonth() === birthdate.getMonth() && today.getDate() < birthdate.getDate()));
+    return age;
 };
 
-const today = new Date();
-const tomorrow = addDays(today, 1);
-const birthday = document.querySelector("#birthday");
+function yourAge () {
+    const age = calculateAge(birthday);
+    document.getElementById("age").value = age;
+    document.getElementById("complete-age").value = age + " ans";
+    }
 
-console.log(today);
-console.log(tomorrow);
+    const today = new Date();
+    const birthday = document.querySelector("#birthday");
 
+    const submit = document.querySelector('.submit-button');
+
+    submit.addEventListener('click', function(button) {
+    const age = calculateAge(birthday);
+    yourAge();
+    });

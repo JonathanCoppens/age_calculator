40dcabe9b2b96a68b5098d75b4a8aaa2f05b3591
Jonathan Coppens <jonathan.b.coppens@gmail.com>
2023-09-11 02:30:10 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+let addDays = function(date, days) {
+    const newDate = new Date(date.getTime());
+    newDate.setDate(newDate.getDate() + days);
+    return newDate;
+};
+
+
+
+let yourAge = function() {
+    newDate
+};
+
+const today = new Date();
+const tomorrow = addDays(today, 1);
+const birthday = document.querySelector("#birthday");
+
+console.log(today);
+console.log(tomorrow);
+
